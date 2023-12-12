d5adeb0038a3e9cf16df42a101cc9b96ea36c54d
Angel Eugenio Charca Blanco <ablanco837@gmail.com>
2023-12-11 23:38:02 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -16,6 +16,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
+import 'core-js-compat';
+
 
 // LightBootstrap plugin
 import LightBootstrap from "./light-bootstrap-main";
